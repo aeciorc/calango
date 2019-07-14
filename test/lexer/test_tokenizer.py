1c794c01ebb855fd6dcbbf1c002181b159ff5c67
aeciorc <aecio.rcv@gmail.com>
2019-07-13 20:59:53 -0400

    added test cases for multiple lines and invalid token

--- test/lexer/test_tokenizer.py
+++ test/lexer/test_tokenizer.py
@@ -16,4 +16,34 @@ class TestTokenizer(unittest.TestCase):
         with patch('lexer.tokenizer.open',
                    new=mock_open(read_data=code_mock)) as _file:
             result = tokenize('path')
-            self.assertListEqual(expected_result[0], result[0])
+            self.assertListEqual(expected_result, result)
+
+    def test_valid_input_multiple_lines(self):
+        code_mock = "echo([1..4])\n" \
+                    "echo(1-1+4)\n"
+
+        expected_result = \
+            [
+                [Token('echo', 'command'), Token('(', 'symbol'), Token('[', 'symbol'), Token('1', 'integer_literal'),
+                 Token('..', 'symbol'), Token('4', 'integer_literal'), Token(']', 'symbol'), Token(')', 'symbol')],
+
+                [Token('echo', 'command'), Token('(', 'symbol'), Token('1', 'integer_literal'),
+                 Token('-', 'symbol'), Token('1', 'integer_literal'), Token('+', 'symbol'),
+                 Token('4', 'integer_literal'), Token(')', 'symbol')]
+            ]
+
+        with patch('lexer.tokenizer.open',
+                   new=mock_open(read_data=code_mock)) as _file:
+            result = tokenize('path')
+            self.assertListEqual(expected_result, result)
+
+    def test_invalid_input(self):
+        code_mock = "echo|1-1+4)\n"
+
+        with patch('lexer.tokenizer.open',
+                   new=mock_open(read_data=code_mock)) as _file:
+            result = None
+            try:
+                result = tokenize('path')
+            except Exception as e:
+                self.assertIsNone(result)
