ed1fdf97715602c4fc77fbf6f1d891b7589078b1
aeciorc <aecio.rcv@gmail.com>
2019-07-14 21:10:49 -0400

    refactored tokenizer.tokenize_line into a much saner and expansible that, refactored token_kinds, created proper excptions, fixed tests

--- test/lexer/test_tokenizer.py
+++ test/lexer/test_tokenizer.py
@@ -1,17 +1,19 @@
 import unittest
 from unittest.mock import mock_open, patch
 
-from lexer.tokenizer import tokenize
+from lexer.tokenizer import tokenize, SyntaxError
+from lexer.token_kinds import *
 from lexer.token import Token
 
 
 class TestTokenizer(unittest.TestCase):
     def test_valid_input_single_line(self):
-        code_mock = "echo([1..4])\n"
+        code_mock = "echo([1 .. 4])\n"
 
         expected_result = [
-            [Token('echo', 'command'), Token('(', 'symbol'), Token('[', 'symbol'), Token('1', 'integer_literal'),
-             Token('..', 'symbol'), Token('4', 'integer_literal'), Token(']', 'symbol'), Token(')', 'symbol')]]
+            [Token('echo', Identifiers.NAME), Token('(', Symbols.NAME), Token('[', Symbols.NAME),
+             Token('1', Numbers.NAME),
+             Token('..', Symbols.NAME), Token('4', Numbers.NAME), Token(']', Symbols.NAME), Token(')', Symbols.NAME)]]
 
         with patch('lexer.tokenizer.open',
                    new=mock_open(read_data=code_mock)) as _file:
@@ -19,17 +21,19 @@ class TestTokenizer(unittest.TestCase):
             self.assertListEqual(expected_result, result)
 
     def test_valid_input_multiple_lines(self):
-        code_mock = "echo([1..4])\n" \
-                    "echo(1-1+4)\n"
+        code_mock = "echo([1 .. 99])\n" \
+                    "echo(1 - 1+ 4)\n"
 
         expected_result = \
             [
-                [Token('echo', 'command'), Token('(', 'symbol'), Token('[', 'symbol'), Token('1', 'integer_literal'),
-                 Token('..', 'symbol'), Token('4', 'integer_literal'), Token(']', 'symbol'), Token(')', 'symbol')],
+                [Token('echo', Identifiers.NAME), Token('(', Symbols.NAME), Token('[', Symbols.NAME),
+                 Token('1', Numbers.NAME),
+                 Token('..', Symbols.NAME), Token('99', Numbers.NAME), Token(']', Symbols.NAME),
+                 Token(')', Symbols.NAME)],
 
-                [Token('echo', 'command'), Token('(', 'symbol'), Token('1', 'integer_literal'),
-                 Token('-', 'symbol'), Token('1', 'integer_literal'), Token('+', 'symbol'),
-                 Token('4', 'integer_literal'), Token(')', 'symbol')]
+                [Token('echo', Identifiers.NAME), Token('(', Symbols.NAME), Token('1', Numbers.NAME),
+                 Token('-', Symbols.NAME), Token('1', Numbers.NAME), Token('+', Symbols.NAME),
+                 Token('4', Numbers.NAME), Token(')', Symbols.NAME)]
             ]
 
         with patch('lexer.tokenizer.open',
@@ -46,4 +50,4 @@ class TestTokenizer(unittest.TestCase):
             try:
                 result = tokenize('path')
             except Exception as e:
-                self.assertIsNone(result)
+                self.assertIsInstance(e, SyntaxError)
